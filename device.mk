8542370573f36129afa2785e557e90b10ce57081
Ivan Vecera <ivan@cera.cz>
2024-12-16 11:52:20 +0100

    veux: Provide camera IDs remapping property and drop camera provider blobs
    Recent change I13a3bd186e7fc48e797e3c6f9039a1dae1fb733f allow to provide
    camera IDs remapping as it is exported from camera module.
    This allows us to drop camera provider blobs.
    
    ref: https://review.lineageos.org/q/topic:%22camera-id-remap%22
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I98b4378cca519aee069d59774c440c850d88d85b

--- device.mk
+++ device.mk
@@ -77,11 +77,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
-# Camera
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
-
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
